fdbef65f2a7e8f4dd0fb8a72453112057a83c134
buyan-kh <vicekurel@gmail.com>
2025-02-06 17:54:20 -0800

    feb 6 update

--- google_sheets_api/server.js
+++ google_sheets_api/server.js
@@ -4,14 +4,14 @@ const app = express();
 const port = 3000;
 
 const sheets = google.sheets('v4');
-const apiKey = 'AIzaSyCSlEvTIIiQL8xZsWAsUv_motGPwzvopuw'; // Replace with your API key
-const spreadsheetId = '1TffM5OKQPmZql3VI7_HsmOFGzPmy14lwdER4uBY6LRE'; // Replace with your spreadsheet ID
+const apiKey = ''; 
+const spreadsheetId = ''; 
 
 app.get('/api/trades', async (req, res) => {
   try {
     const response = await sheets.spreadsheets.values.get({
       spreadsheetId,
-      range: 'Sheet1!A2:I', // Adjust the range as needed
+      range: 'Sheet1!A2:I', 
       key: apiKey,
     });
 
